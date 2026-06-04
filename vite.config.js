b762d5b248dee321197d21adb817dfaa9cf5b0c8
Lokre <franciscomaureirap@gmail.com>
2026-06-03 22:16:09 -0400

    Hot fix: cambio a dominio default

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // For custom domain (proyecta.cai.cl), base should be '/'
-  // If deploying to username.github.io/repo-name, change to '/repo-name/'
-  base: "/",
+  base: "/Proyecta/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
